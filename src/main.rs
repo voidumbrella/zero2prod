fd06eb8a59d0c499277b7cee362ddc2e1828289b
voidumbrella <voidumbrella@gmail.com>
2025-02-08 20:11:18 -0500

    Deploy to DigitalOcean

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,12 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind to port.");
     run(listener, connection_pool)?.await
 }
